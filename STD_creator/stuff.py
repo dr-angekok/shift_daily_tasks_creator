593fcc1c5cecde5a5a77e4f55afea3ad994e18d5
Angekok <angekok@mail.ru>
2022-01-03 22:52:08 +0300

    Add random name choice.

--- STD_creator/stuff.py
+++ STD_creator/stuff.py
@@ -1,6 +1,9 @@
 import pandas as pd
+from collections import Counter
+from random import choice
 
-COLUMN ={
+
+COLUMN = {
     'NAMES': 'name',
     'PROFESSION': 'prof',
     'NUMBER': 'numb',
@@ -12,15 +15,26 @@ COL_COMPARISON = {
     'Табельный номер': COLUMN['NUMBER'],
 }
 
+DEF_COMPARISON = {
+    'токарная': 'токарь',
+    'фрезерная': 'фрезеровщик',
+    'шлифовальная': 'шлифовщик',
+    'расточная': 'токарь-расточник',
+    'слесарная': 'слесарь-инструментальщик',
+}
+
 
 class StaffingTable:
-    def __init__(self, filename):
+    def __init__(self, filename, comparison=DEF_COMPARISON):
         loaded_base = pd.read_excel(filename)
         loaded_base.dropna(inplace=True)
+        self.comparison = comparison
         
         self.base = pd.DataFrame()
         for key in COL_COMPARISON.keys():
             self.base[COL_COMPARISON[key]] = loaded_base[key]
+        self.base[COLUMN['PROFESSION']] = self.base[COLUMN['PROFESSION']].apply(self._prof_clear)
+        self.base.dropna(inplace=True)
 
 
     @property
@@ -30,3 +44,20 @@ class StaffingTable:
     @property
     def columns_count(self):
         return self.base.shape[0]
+
+    @property
+    def professions_count(self):
+        return Counter(self.base[COLUMN['PROFESSION']])
+
+    def _prof_clear(self, item):
+        if item in self.comparison.values():
+            return item
+        return pd.NaT
+
+    def get_names(self, profession):
+        lines = self.base[self.base[COLUMN['PROFESSION']] == profession]
+        clear_lines = lines.drop(COLUMN['PROFESSION'], axis=1)
+        return clear_lines.values
+
+    def get_rnd_names_set(self, profession):
+        return choice(self.get_names(profession))
--- tests/test_stuffer.py
+++ tests/test_stuffer.py
@@ -13,4 +13,9 @@ def test_stuffing():
     base = StaffingTable(STUFF_PATH)
     for col in base.columns:
         assert col in COLUMN.values()
-    assert base.columns_count == 69
+    assert base.columns_count == 40
+    assert 'токарь' in base.professions_count
+    tested_names = base.get_names('токарь')
+    assert 'Лавров Владимир Михайлович' in tested_names
+    assert base.get_rnd_names_set('токарь')[0] in tested_names
+
